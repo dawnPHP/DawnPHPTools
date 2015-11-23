97f784071d5e95c20e8f51ccfd64e2bec5193200
Dawn <JimmyMall@live.com>
2015-11-23 20:45:59 +0800

    (2.2)insert tags into db

--- git_short_tags/tags/action.php
+++ git_short_tags/tags/action.php
@@ -14,45 +14,88 @@ switch($_GET['a']){
 	case 'tag':
 		$tags=$_POST['tags'];
 		$a_id=$_POST['a_id'];//文章id
-		$uid=1;//从session中读取用户id
+		$u_id=$_POST['u_id'];//从session中读取用户id
 		$aTag=explode(',',$tags);
 		print_r($aTag);
-		addTagsToDb($aTag,$uid,$a_id);
+		addTagsToDb($aTag,$u_id,$a_id);
+		break;
+	default:
+		break;
 }
+
+
+
+//=================================
 
 /**
 	把提交的标签插入到新的mysql中
 */
-function addTagsToDb($aTag,$uid,$a_id){
+function addTagsToDb($aTag,$u_id,$a_id){
 	//从tag表获取该用户的所有标签
-	$allTags=getTagsBy('uid',$uid);
-
+	$allTags=getTagsBy($u_id);
+	//在关联表中 删除aid的标签
+	$sql="delete from article_tags where a_id='{$a_id}'";
+	mysql_query($sql) or die('Delete Err:'.mysql_error());
+	
+	//添加标签的逻辑
 	for($i=0;$i<count($aTag);$i++){
 		$tag=$aTag[$i];
 		//1.是否重复
-		if( in_array($tag,$allTags) ){ 
-			continue;//重复的不再处理
+		$result=in_array2($tag,$allTags);
+		if( false==$result ){
+			//否则添加到tags表中
+			$sql="insert into tags(u_id,tag) values('{$u_id}','{$tag}')";
+			mysql_query($sql) or die(mysql_error());
+			$t_id=mysql_insert_id();
+			//echo 'no';print_r($t_id);die();//no2
+			//向关联表添加数据
+			addTo_tb_Assoc($a_id,$t_id);
+			
 		}else{
-			//否则添加到数据库中
-			$sql="insert into tags(u_id,tag) values('{$uid}','{$tag}')";
+			//重复的不再添加到tags表中  //print_r($arr);die();//todo
+			$t_id=$result[0];
+			//echo '~~old~~'.$t_id; print_r($tag);die();
+			//向关联表添加数据
+			addTo_tb_Assoc($a_id,$t_id);
 		}
-		//2.是否合适
-		
 	}
-	
-
 }
+
+/**
+	向article_tags表中添加数据
+*/
+function addTo_tb_Assoc($a_id,$t_id){
+	$sql="insert into article_tags(a_id, t_id) values('{$a_id}','{$t_id}')";
+	mysql_query($sql) or die(mysql_error());
+}
+
+
+/**
+	检测$tag是否在$allTags[1]中
+*/
+function in_array2($tag,$allTags){
+	for($i=0;$i<count($allTags);$i++){
+		if($allTags[$i][1]==$tag){
+			return array( $allTags[$i][0] );
+		}
+	}
+	return false;
+}
+
+
 
 /**
 	从tag表获取该用户的所有标签
 */
-function getTagsBy($key,$value){
-	$sql="select * from tags where $key=$value";
+function getTagsBy($u_id){
+	$sql="select id,tag from tags where u_id='{$u_id}'";
 	$rows=mysql_query($sql);
+
 	$arr=array();
 	while( $row=mysql_fetch_assoc($rows) ){
-		$arr[]=$row['tag'];
+		$arr[]=array($row['id'],$row['tag']);
 	}
+	//print_r($arr);die();
 	return $arr;
 }
 
@@ -65,14 +108,16 @@ function getTagsBy($key,$value){
 	if(tag not in tableTags){
 		添加到tags表；
 		获取tag的id；
-		插入到tags表中；
+		插入到关联表中；
 		
 	}else{
 		获取tag的id；
-		插入到tags表中；
+		插入到关联表中；
 	}
 }
 
+
+truncate table table_name;
 */
 
 /*
