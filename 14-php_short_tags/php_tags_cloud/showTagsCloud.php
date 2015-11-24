f76756a961213992db9d9729470208f2fe57369b
Dawn <JimmyMall@live.com>
2015-11-24 13:43:01 +0800

    add one function

--- 14-php_short_tags/php_tags_cloud/showTagsCloud.php
+++ 14-php_short_tags/php_tags_cloud/showTagsCloud.php
@@ -13,9 +13,9 @@ function getCloud( $data = array(), $minFontSize = 12, $maxFontSize = 50 ){
 		$size = $minFontSize + ( $count - $minimumCount )
 				* ( $maxFontSize - $minFontSize ) / $spread;
 		$cloudTags[] = '<a style="font-size: ' . floor( $size ) . 'px'
-					. '" href="index.php?tag='.stripslashes( $tag ).'" title="\'' . $tag .
+					. '" href="../tags/index.php?tag='.stripslashes( $tag ).'" title="\'' . $tag .
 					'\' returned a count of ' . $count . '">'
-					. htmlspecialchars( stripslashes( $tag ) ) . '</a>';
+					. htmlspecialchars( stripslashes( $tag ) ) .'('.$count.')' . '</a>'.'&nbsp;&nbsp;';
 	} 
 	 
 	return join( "\n", $cloudTags ) . "\n";
@@ -29,21 +29,29 @@ $arr = Array('Actionscript' => 35, 'Adobe' => 22, 'Array' => 44, 'Background' =>
 'Extract' => 28, 'Filters' => 12);
 echo getCloud($arr, 12, 36); 
 */
+
+//排除函数
 function debug($a){
 	echo '<pre>';
 	print_r($a);
 	echo '</pre>';
 	die();
 }
-include '../tags/common/conn.php';
 
-$sql="select tag, count(t_id) as num from tags a,article_tags b where a.id=b.t_id and a.u_id='1' group by tag;";//改变用户名id
 
-$rows=mysql_query($sql) or die(mysql_error());
-$myArr=array();
-while( ($row=mysql_fetch_assoc($rows)) !=null){
-	$myArr[$row['tag']]=$row['num'];
+//获取标签/计数信息
+function getTagsCount($u_id){
+	$sql="select tag, count(t_id) as num from tags a,article_tags b where a.id=b.t_id and a.u_id='{$u_id}' group by tag;";//改变用户名id
+
+	$rows=mysql_query($sql) or die(mysql_error());
+	$myArr=array();
+	while( ($row=mysql_fetch_assoc($rows)) !=null){
+		$myArr[$row['tag']]=$row['num'];
+	}
+	return $myArr;
 }
 
+//
+include '../tags/common/conn.php';
+$myArr=getTagsCount(1);
 echo getCloud($myArr, 12, 30); 
-
