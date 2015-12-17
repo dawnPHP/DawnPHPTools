10e6aca68ed72a2a4e9fdcc0828940f40195895a
Dawn <JimmyMall@live.com>
2015-12-17 11:05:24 +0800

    17th tool: 003 can get_QRCode_image

--- 17-php_weixin/mytest.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-include('DawnPHP/class/MyDebug.class.php');
-
-MyDebug::f($_POST,'POST.TXT');
-
-$data = file_get_contents("php://input");
-
-MyDebug::f($data,'post2.txt');
-echo 'done' . time();
