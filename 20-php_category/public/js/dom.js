78579b20dec4d72ea52b8893523e0c32d00380b5
Dawn <JimmyMall@live.com>
2016-01-22 22:14:44 +0800

    v0.1.9.3: createElement(); not display prop after add all prop

--- 20-php_category/public/js/dom.js
+++ 20-php_category/public/js/dom.js
@@ -37,6 +37,30 @@ function isEmpty(obj,desc){
 	return false;
 }
 
+/**
+	根据json信息建立dom元素
+	@para string 要建立的标签
+	@para json object 属性键值对
+	@para string or dom 元素内部innerHTML
+	return dom元素
+*/
+function createElement(tag,json,innerHTML){
+	var innerHTML=innerHTML||'';
+	//新建对象
+	var dom=document.createElement(tag);
+	//添加属性
+	for(var key in json){
+		dom.setAttribute(key,json[key]);
+	}
+	//塞入内容
+	if(innerHTML!=''){
+		dom.innerHTML=innerHTML;
+	}
+	//返回
+	return dom;
+}
+//测试 getElement('div',{'class':'red', 'id':'test2', },'123');
+
 
 
 //==============================	
